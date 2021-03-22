3c6377aa0046cfca34fb283b7c4349908d27119d
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-03-22 14:51:03 +0200

    Update game.js

--- game.js
+++ game.js
@@ -52,11 +52,11 @@ function addLastItems() {
   createItem(430, 291, 'letter-red');
   createItem(300, 241, 'letter-red');
   // third hall items
-  createItem(690, 447, 'letter-red');
+  createItem(680, 447, 'letter-red');
   createItem(548, 363, 'letter-red');
-  createItem(690, 228, 'letter-red');
+  createItem(680, 228, 'letter-red');
   createItem(548, 220, 'letter-red');
-  createItem(662, 135, 'letter-red');
+  createItem(662, 175, 'letter-red');
   createItem(548, 84, 'letter-red');
 
 }
