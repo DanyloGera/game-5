77e8cc9fb3ea88795b8070e8fed1caab9b7f63d4
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-03-22 10:49:29 +0200

    Update game.js

--- game.js
+++ game.js
@@ -17,8 +17,8 @@ function addItems() {
   items = game.add.physicsGroup();
 
   createItem(220, 333, 'letter');
-  createItem(305, 184, 'letter');
-  createItem(410, 341, 'letter');
+  createItem(305, 204, 'letter');
+  createItem(410, 361, 'letter');
 
 }
 
