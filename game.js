38f7007b8d248565a857e2eb8812eb82ca6bfe9c
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-03-28 19:05:08 +0300

    Update game.js

--- game.js
+++ game.js
@@ -171,12 +171,12 @@ window.onload = function () {
     game.load.spritesheet('player', 'chalkers.png',  48, 62 );
 
 
-    game.load.spritesheet('letter1', 'blue.png', 36, 24);
-    game.load.spritesheet('letter2', 'blue.png', 36, 24);
-    game.load.spritesheet('letter3', 'blue.png', 36, 24);
-    game.load.spritesheet('letter4', 'blue.png', 36, 24);
-    game.load.spritesheet('letter-blue', 'blue.png', 36, 24);
-    game.load.spritesheet('letter-red', 'red.png', 36, 24);
+    game.load.spritesheet('letter1', 'blue.png', 36, 44);
+    game.load.spritesheet('letter2', 'blue.png', 36, 44);
+    game.load.spritesheet('letter3', 'blue.png', 36, 44);
+    game.load.spritesheet('letter4', 'blue.png', 36, 44);
+    game.load.spritesheet('letter-blue', 'blue.png', 36, 44);
+    game.load.spritesheet('letter-red', 'red.png', 36, 44);
 
 
     game.load.spritesheet('badge', 'badge.png', 42, 54);
