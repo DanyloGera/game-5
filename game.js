d84d189379d2a4ef276a144e8fba2c8bee00cc5e
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-03-28 12:12:30 +0300

    Update game.js

--- game.js
+++ game.js
@@ -139,7 +139,6 @@ function itemHandler(player, item) {
 // when the player collects the badge at the end of the game
 function badgeHandler(player, badge) {
   badge.kill();
-  items.kill();
   var btn = document.getElementById('btn-next');
   btn.removeAttribute("disabled");
   btn.style.border = "2px solid #000";
