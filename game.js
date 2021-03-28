e787d8a83fb2e7365ff6fdd431db91fd104c7eec
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-03-28 11:27:48 +0300

    btn

--- game.js
+++ game.js
@@ -139,7 +139,10 @@ function itemHandler(player, item) {
 // when the player collects the badge at the end of the game
 function badgeHandler(player, badge) {
   badge.kill();
-  // won = true;
+  var btn = document.getElementById('btn-next');
+  btn.removeAttribute("disabled");
+  btn.style.border = "2px solid #000";
+  won = true;
 }
 
 // setup game when the web page loads
