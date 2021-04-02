cf68f26b6a6fe8e8543d4a09f45fb8fc10724c47
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-04-02 17:50:09 +0300

    Update game.js

--- game.js
+++ game.js
@@ -149,13 +149,13 @@ addStar();
 }
 
 // when the player collects the badge at the end of the game
-// function badgeHandler(player, badge) {
-//   badge.kill();
-//   var btn = document.getElementById('btn-next');
-//   btn.removeAttribute("disabled");
-//   btn.style.border = "2px solid #000";
-//   won = true;
-// }
+function badgeHandler(player, badge) {
+  badge.kill();
+  var btn = document.getElementById('btn-next');
+  btn.removeAttribute("disabled");
+  btn.style.border = "2px solid #000";
+  won = true;
+}
 
 // setup game when the web page loads
 window.onload = function () {
